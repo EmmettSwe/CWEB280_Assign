6c1c2db6e52d6ee2794aed95518e29e6931fe22c
Swejda9940 <Swejda9940@saskpolytech.ca>
2024-10-03 14:01:10 -0600

    Added all the routers that we should need

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const searchRouter = require('./routes/Search')
+const SearchRouter = require('./routes/Search')
+const BookshelfRouter = require('./routes/Bookshelf')
+const AboutRouter = require('./routes/About')
+
 
 
 var app = express();
@@ -21,9 +22,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/Search', searchRouter)
+app.use('/', SearchRouter)
+app.use('/Search', SearchRouter)
+app.use('/Bookshelf', BookshelfRouter)
+app.use('/About', AboutRouter)
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
