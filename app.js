f153920e5d7d62498625b09a2b18d9c3ada1bd3f
Drew Glasser-Brown <glasserbrown6602@saskpolytech.ca>
2024-10-06 13:23:33 -0600

    Added Search route functionality

--- app.js
+++ app.js
@@ -20,10 +20,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/', SearchRouter)
-app.use('/Search', SearchRouter)
 app.use('/Bookshelf', BookshelfRouter)
 app.use('/About', AboutRouter)
 
--- routes/Search.js
+++ routes/Search.js
@@ -2,8 +2,34 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',  function(req, res, next) {
     res.render('Search_Form', { title: 'Search Form' });
 });
+
+/* POST handler for http://localhost:3000/ */
+router.post('/search', async function(req, res, next) {
+
+    let data = await fetchData(req.body.sMethod, req.body.search);
+
+    res.render('Search_Result',
+        {
+            pageTitle: `Search results for ${req.body.search}`,
+            data
+        });
+})
+
+const fetchData =  (method, search) => {
+    let string = search.replaceAll(" " , "+")
+    return fetch(`https://openlibrary.org/search.json?${method}=${string}`)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("Something went wrong");
+            }
+            return res.json();
+        })
+        .then(json => {
+            return json.docs;
+        })
+}
 
 module.exports = router;
