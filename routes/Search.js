3b0251879d2615fffeaa47fffd282b7ed6210d02
Swejda9940 <Swejda9940@saskpolytech.ca>
2024-10-06 15:44:03 -0600

    validation broken cant display error msgs

--- routes/Search.js
+++ routes/Search.js
@@ -1,5 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const multer = require('multer')
+const uploader = multer(
+    {
+        dest: 'public/uploads/',
+        fileFilter: (req, file, callback) => {
+            // add a filter to the mime type so only image files will be accepted
+            if (file.mimetype.startsWith('image/')) {
+                callback(null, true) // null means no error message, true means allowed
+            } else {
+                // make new error with message, and false means not allowed
+                return callback(new Error('Only images are allowed'), false)
+            }
+        },
+        limits: {
+            fileSize: 2 * 1024 * 1024 // 2mB MAX FILE SIZE
+        }
+    })
+const fs = require('fs')
+// eslint-disable-next-line no-unused-vars
+
+// include code from express validator package
+const { body, query, validation, validationResult } = require('express-validator')
+// Create a formatter to clean up the validation result data in a way that makes it easier to display to the user
+const onlyMsgErrorFormatter = ({ location, msg, param, value, nestedErrors }) => {
+    return msg // we only want the message from All the params being sent in to the formatter
+}
+
 
 /* GET home page. */
 router.get('/',  function(req, res, next) {
@@ -36,4 +63,30 @@ const fetchData =  (method, search) => {
 router.get('/Review',  function(req, res, next) {
     res.render('Review', { title: 'Review Form' });
 });
+router.post('/Review',
+    [
+        body('name').trim().notEmpty().withMessage('You must enter a pen name').bail()
+            .isLength({min: 3, max: 20}).withMessage('Pen name must be between 3 and 20')
+
+    ],
+    (req, res) => {
+        const violations = validationResult(req)
+        console.log('Validation Results Violations')
+        console.log(violations)
+
+        // take the ugly violations onj and clean it up for display
+        const errorMessages = violations.formatWith(onlyMsgErrorFormatter).mapped()
+
+        console.log(errorMessages)
+
+        res.render("Review",{
+            title: 'Review form',
+            name: req.body.name,
+            err: errorMessages
+        })
+    })
+
+
+
+
 module.exports = router;
