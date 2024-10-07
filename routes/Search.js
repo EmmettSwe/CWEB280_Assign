543e3a2a166bd4a6b55631c39d469d4656bef42d
Drew Glasser-Brown <glasserbrown6602@saskpolytech.ca>
2024-10-06 21:49:13 -0600

    Fixed image uploading and validation in Bookshelf, fixed Search and Search_Form, as well as Review.hbs

--- routes/Search.js
+++ routes/Search.js
@@ -1,36 +1,15 @@
 var express = require('express');
 var router = express.Router();
-const multer = require('multer')
-const uploader = multer(
-    {
-        dest: 'public/uploads/',
-        fileFilter: (req, file, callback) => {
-            // add a filter to the mime type so only image files will be accepted
-            if (file.mimetype.startsWith('image/')) {
-                callback(null, true) // null means no error message, true means allowed
-            } else {
-                // make new error with message, and false means not allowed
-                return callback(new Error('Only images are allowed'), false)
-            }
-        },
-        limits: {
-            fileSize: 2 * 1024 * 1024 // 2mB MAX FILE SIZE
-        }
-    })
-const fs = require('fs')
-// eslint-disable-next-line no-unused-vars
 
-// include code from express validator package
+// include code from express validator
 const {body, query, validation, validationResult} = require('express-validator')
 const onlyMsgErrorFormatter = ({location, msg, param, value, nestedErrors}) => {
     return msg // we only want the message from All the params being sent in to the formatter
 }
 
-
-/* GET home page. */
-router.get('/search', function (req, res, next) {
-
-
+/* GET handler for http://localhost:3000/ and http://localhost:3000/search/. */
+router.get('/', function (req, res, next) {
+    // Just render the form
     res.render('Search_Form', {
         title: 'Search Form'
     });
@@ -42,7 +21,6 @@ router.post('/search',
     [
         body('sMethod').notEmpty().withMessage('You must use a search method').bail()
             .custom((value, {req}) => {
-                console.log("this is s method" + req.body.sMethod)
                 if (req.body.sMethod === 'title' || req.body.sMethod === 'author') {
                     return true
                 } else {
@@ -53,53 +31,70 @@ router.post('/search',
                 passed1 = true
                 return true
             }),
+        // Validation the search field
         body('search').notEmpty().withMessage('You must enter a value to search by').bail()
             .isLength({
                 min: 1,
-                max: 20
-            }).withMessage('A search value must be no less than 1 and no greater than 20').bail()
+                max: 50
+            }).withMessage('A search value must be no less than 1 and no greater than 50').bail()
             .custom((value, {req}) => {
+                // if everything passed then true
                 passed = true
-                console.log("I Am true the second one ")
                 return true
             })
     ],
+    // Because we are using a fetchData method the function needs to be asynchronous
     async function (req, res, next) {
+        // check for valid input data
         const violations = validationResult(req)
-        console.log('Validation Results Violations')
-        console.log(violations)
 
-        // take the ugly violations onj and clean it up for display
+        // Creating errorMessages object
         const errorMessages = violations.formatWith(onlyMsgErrorFormatter).mapped()
 
-        console.log(errorMessages)
+
+        // If validation passed then we fetch the data and render the Search results
         if (passed && passed1) {
+            // We call await so that the application knows not to move on
+            // until it gets the data from the fetchData method
             let data = await fetchData(req.body.sMethod, req.body.search);
             res.render('Search_Result',
                 {
+                    // Title is dynamic to what they searched so that they remember
                     pageTitle: `Search results for ${req.body.search}`,
+                    // We use syntactic sugar to have data: data
                     data
                 });
         } else {
+            // Pass the errors into the form
             res.render('Search_Form', {
                 title: "Search Form",
                 search: req.body.search,
                 err: errorMessages
             })
         }
-
     })
 
+/**
+ * This method will return fetched data from the OpenLibrary API
+ * @param method string of either "title" or "author" of which to query
+ * @param search string of the value to search by
+ * @returns {Promise<any>} the docs object which is returned from OpenLibrary and contains the search data
+ */
 const fetchData = (method, search) => {
+    // fixing the search value so that it can be passed into the api request
     let string = search.replaceAll(" ", "+")
+    // this template string takes in the query for method=searchValue
     return fetch(`https://openlibrary.org/search.json?${method}=${string}`)
         .then(res => {
+            // Making sure that we got the response
             if (!res.ok) {
                 throw new Error("Something went wrong");
             }
+            // passing the parsed json to the next method
             return res.json();
         })
         .then(json => {
+            // Overall returning the "docs" object from the API
             return json.docs;
         })
 }
